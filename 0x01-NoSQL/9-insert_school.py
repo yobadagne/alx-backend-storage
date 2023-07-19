9e86c1bea521da525bf631ed1d900c47b9d50c72
yobadagne <yobadagne@gmail.com>
2023-07-19 09:10:07 +0000

    9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" module have a utility function that insert documents
+"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    insert into school
+    """
+    return mongo_collection.insert_one(kwargs).inserted_id
