7cf383d60d272a7343a95e00e2d51c2b3a900bb5
yobadagne <yobadagne@gmail.com>
2023-07-19 09:19:51 +0000

    11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Find by topic
+    """
+    return mongo_collection.find({"topics": topic})
