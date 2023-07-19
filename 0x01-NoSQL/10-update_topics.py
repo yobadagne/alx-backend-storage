9feb4dbf431e96bba63ce879165dbab8fbcfdb65
yobadagne <yobadagne@gmail.com>
2023-07-19 09:15:14 +0000

    10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+"""
+update school topic
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update many rows
+    """
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+ )
